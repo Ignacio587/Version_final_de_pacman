2a04d400990ba5ad484a1a3cfc3a6dbf89ff80af
Ignacio587 <icontardo2024@udec.cl>
2024-11-27 23:18:16 -0300

    Update imprimir.h

--- imprimir.h
+++ imprimir.h
@@ -2,7 +2,7 @@
 #ifndef IMPRIMIR_H
 #define IMPRIMIR_H
 void ImprimirMatriz(int** matriz, int nFilas, int nColumnas);
-void ImprimirInfo(int puntaje, int tiempo, int vidas);
+void ImprimirInfo(int puntaje, int tiempo, int vidas, int niveles);
 void ImprimirGameOver(int puntaje);
 
 #endif
